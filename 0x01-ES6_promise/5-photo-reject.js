1e80d966fbe519e6b530b666b1b57f40518f8bc1
nathen321 <walidjalaly43@gmail.com>
2025-04-17 16:49:34 +0000

    update task 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(new Error(`${filename} cannot be processed`)).catch(console.error);
+  return Promise.reject(
+    new Error(`${filename} cannot be processed`),
+  );
 }
