6e293cc1258b7a2d9a55c49520300176f36685a0
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-04-18 00:40:58 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  result.map((res) => [
-    {
-      status: res.status,
-      value: res.status === 'fulfilled' ? res.value : String(res.reason),
-    },
-  ]);
-  return result;
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
