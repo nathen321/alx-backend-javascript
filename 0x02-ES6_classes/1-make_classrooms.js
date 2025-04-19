2f1f3cd92d6adea500d118e01f6a1ec2cedbfd7b
nathen321 <walidjalaly43@gmail.com>
2025-04-19 09:47:50 +0000

    corection of class

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
-import Classroom from './0-classroom';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
+export default function initializeRooms() {
   return [
-    new Classroom(19),
-    new Classroom(20),
-    new Classroom(34),
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
   ];
 }
-
-export default initializeRooms;
