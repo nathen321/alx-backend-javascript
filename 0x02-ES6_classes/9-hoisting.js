3f39c1e482bd4a4f017451af07eec8f5cae3180a
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-04-20 19:31:48 +0100

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -42,4 +42,5 @@ const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
