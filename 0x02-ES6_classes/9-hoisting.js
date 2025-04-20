1e3eb85f53b16e6d72b8445e4321a32cb45db275
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-04-20 19:09:30 +0100

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,6 +1,3 @@
-const class2019 = new ALXClass(2019, 'San Francisco');
-const class2020 = new ALXClass(2020, 'San Francisco');
-
 export class ALXClass {
   constructor(year, location) {
     this._year = year;
@@ -15,12 +12,6 @@ export class ALXClass {
     return this._location;
   }
 }
-
-const student1 = new StudentALX('Guillaume', 'Salva', class2020);
-const student2 = new StudentALX('John', 'Doe', class2020);
-const student3 = new StudentALX('Albert', 'Clinton', class2019);
-const student4 = new StudentALX('Donald', 'Bush', class2019);
-const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
 export class StudentALX {
   constructor(firstName, lastName, alxClass) {
@@ -41,6 +32,14 @@ export class StudentALX {
     return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
+
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
+const student1 = new StudentALX('Guillaume', 'Salva', class2020);
+const student2 = new StudentALX('John', 'Doe', class2020);
+const student3 = new StudentALX('Albert', 'Clinton', class2019);
+const student4 = new StudentALX('Donald', 'Bush', class2019);
+const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
