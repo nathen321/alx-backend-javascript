a3fea86813379acc41dae3051284de0bbf79eaf5
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-04-20 18:16:24 +0100

    Create 8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,33 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+     switch (hint) {
+      case 'string':
+        return this.location;
+      case 'number':
+        return this.size;
+      case 'default':
+        return this;
+    }
+  }
+}
