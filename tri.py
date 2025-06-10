483019492413bafc2eb7f01b353862f988c766cd
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-10 22:44:05 +0100

    test ting
    testing stuf

---
+++ tri.py
@@ -0,0 +1,37 @@
+def pascal_triangle(n):
+    """Generate Pascal's Triangle with n rows.
+
+    Args:
+        n (int): Number of rows to generate in the triangle.
+
+    Returns:
+        list of lists: A list of lists representing Pascal's Triangle.
+                       Returns a list containing an empty list if n <= 0.
+
+    Examples:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    """
+    if n <= 0:
+        return [[]]
+    tri = [[1]]
+    while (n > 1):
+        row = []
+        for i in range(len(tri[-1])):
+            if i == 0:
+                row.append(1)
+            else:
+                row.append(tri[-1][i - 1] + tri[-1][i])
+        row.append(1)
+        tri.append(row)
+        n -= 1
+    return tri
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+print_triangle(pascal_triangle(0))
