432b38203a66217ee5cf6374aff47fe4aee8ca80
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-11 00:49:05 +0100

    refactor: improve code formatting and documentation for displayMessage function

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,9 @@
-// 0-console.js
-
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
-
 
 module.exports = displayMessage;
