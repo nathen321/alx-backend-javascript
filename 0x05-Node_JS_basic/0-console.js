cadfcad85f6840d3910842309479f3308a338a2c
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-07-08 23:40:35 +0100

    refactor: update displayMessage function to use arrow function syntax and improve documentation

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,15 @@
 /**
- * Displays a message on the STDOUT.
- * @param {String} message The message to display.
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
  */
-function displayMessage(message) {
-  console.log(message);
-}
+// Arrow function
+const displayMessage = (message) => console.log(message);
 
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
 module.exports = displayMessage;
